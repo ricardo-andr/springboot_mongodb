95f4a224617273bed93765cc35ccb4061ed03e94
ricerdr1 <ricardoandr19@gmail.com>
2021-04-01 14:01:08 -0300

    Primeiro envio

---
+++ src/test/java/com/crousemongodb/Workshopmongo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crousemongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workshopmongo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
